1b7e3acb319fc0ba9f9ce29cc1d2b616524a486d
Zahid <zahid67635gmail.com>
2023-07-25 22:51:20 +0600

    header added with an animation

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
 
 const Main = () => {
     return (
         <div>
             <Navbar />
-            <Outlet />
+            <div className='w-5/6 mx-auto pb-4'>
+                <Outlet />
+            </div>
+            <Footer />
         </div>
     );
 };
