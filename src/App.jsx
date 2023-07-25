1d06531e293a544c0b81f5ebbedd70e616a875cc
Zahid <zahid67635gmail.com>
2023-07-25 17:02:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Navbar from './Components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1 className='text-3xl'>mine</h1>
+    </>
+  )
+}
+
+export default App
