0041aa9f2252d7e14f97f41116c3411f2d1a4334
Zahid <zahid67635gmail.com>
2023-07-27 15:26:09 +0600

    admin all products view added

--- src/Pages/Dashboard/AllProducts.jsx
+++ src/Pages/Dashboard/AllProducts.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+import ProductTable from "../../Components/ProductTable";
 
 const AllProducts = () => {
-    return (
-        <div>
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch(`https://fakestoreapi.com/products/`)
+            .then(res => res.json())
+            .then(data => setProducts(data))
+            .catch(er => console.log(er))
+    }, [products.length])
 
+    return (
+        <div className="overflow-x-auto">
+            <table className="table">
+                {/* head */}
+                <thead>
+                    <tr className="text-base text-violet-600">
+                        <th>ID</th>
+                        <th>Product Name & Photo</th>
+                        <th>Price</th>
+                        <th>Category</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                {
+                    products.map(p => <ProductTable key={p.id} product={p} />)
+                }
+            </table>
         </div>
     );
 };
