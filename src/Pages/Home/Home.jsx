27d6bd6dba23e09a7790c0a16e0a8d8c04126770
Zahid <zahid67635gmail.com>
2023-07-28 16:20:19 +0600

    adjust the buttons

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -15,12 +15,11 @@ const Home = () => {
 
                             Find Your Product with <span className='text-sky-400'>E-Commerce</span> Now!!
                         </h1>
-                        <p className="mt-6 mb-8 text-lg sm:mb-12 font-semibold">
+                        <p className="mt-6 mb-6 text-lg sm:mb-12 font-semibold">
                             <span className='text-sky-500'>E-Commerce</span>, Your one-stop destination for seamless online shopping and unbeatable deals
                         </p>
                         <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-                            <Link to='/products' rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded bg-sky-600 hover:bg-sky-700 text-gray-50">SHOP</Link>
-                            <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold border rounded border-gray-800 hover:text-sky-600">Categories</a>
+                            <Link to='/products' rel="noopener noreferrer" href="#" className="px-6 md:px-10 py-3 md:py-2 text-lg font-semibold rounded bg-sky-600 hover:bg-sky-700 text-gray-50">SHOP</Link>
                         </div>
                     </div>
                     <div className="flex items-center justify-center md:p-6 md:mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
